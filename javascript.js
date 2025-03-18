2029ee28465698736fab3b9ebfc6ea7ddad22d77
MichaelAK-16503 <krad16503@gmail.com>
2025-03-18 12:41:58 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,56 @@
+// Variables
+// 1. let
+// 2. var
+// 3. const
+// These Variables are used to store a data in a names/letters.
+
+// Condition Statement
+// 1. if
+// 2. else
+// 3. elseif
+// These Condition Statements are used to identify the answer (whether it is true or false).
+
+// Looping Statement
+// 1. For Loop
+//
+
+// Functions
+// 1. Named function
+// 2. Expression functiom
+// 3. Arrow function
+// Syntax of Functions
+// 1. Named Function Syntax
+// function funname() {
+//     console.log(a + b);
+// }
+// 2. Expression Function Syntax
+// let cc = function () {
+//     console.log('eygueiir');
+// }
+// 3. Arrow Function Syntax
+// let xx = () => {
+//     console.log("hello everyone");
+// }
+// object
+// syntax
+// let objname={
+//   name:key,
+//   name:key,
+// }
+// storage
+// 1. localStorage
+// 2. session storage
+// Syntax of Storage
+// localStorage.setItem("name", "sabesh");
+// let uu = localStorage.getItem("name");
+// console.log(uu);
+// document.getElementById("heading").innerHTML = uu;
+// localStorage.removeItem("name");
+// Array
+// Syntax
+// let arr[1,2,3,4,5,6,7]
+// console.log(arr);
+// 1. Push : Add the data at last
+// 2. Pop  : Delete the data at last
+// 3. Shift : Add the data at first
+// 4. Unshift : Delete the data at first 
---
+++ operators.js
@@ -0,0 +1,33 @@
+// Arithmetic Operators
+// +  Addition
+// -  Subtraction
+// *  Multiplication
+// ** Exponentiation (ES2016)
+// /  Division
+// %  Modulus (Division Remainder)
+// ++ Increment
+// -- Decrement
+
+//  Assignment Operators
+// =    x = y   x = y
+// +=   x += y  x = x + y
+// -=   x -= y  x = x - y
+// *=   x *= y  x = x * y
+// /=   x /= y  x = x / y
+// %=   x %= y  x = x % y
+// **=      y   x = x ** y
+
+// Comparison Operators
+// ==   Equal to
+// ===  Equal value and Equal Type
+// !=   Not Equal
+// !==  Equal value and Equal Type
+// >    Greater than
+// <    Less than
+// >=   Greater than or Equal to
+// <=   Less than or Equal to
+
+//  Logical Operator
+// &&   Logical And
+// ||   Logical Or
+// !    Logical Not
